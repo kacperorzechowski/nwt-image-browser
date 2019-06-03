360dd0ff69728752f22996cc6499318da72faeeb
Kacper Orzechowski <kacper.orzechowski96@gmail.com>
2019-06-03 08:33:32 +0200

    add search images feature update readme

--- src/components/VSearch.vue
+++ src/components/VSearch.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <v-search-icon class="search-icon" @click="search"></v-search-icon>
+    <v-search-icon class="search-icon" @click.native="search"></v-search-icon>
     <input
       @keyup.enter="search"
       v-model="phrase"
@@ -14,6 +14,8 @@
 
 <script>
 import VSearchIcon from '@/components/VSearchIcon'
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'VSearch',
   components: { VSearchIcon },
@@ -21,8 +23,16 @@ export default {
     phrase: null
   }),
   methods: {
+    ...mapMutations({
+      setPhrase: 'gallery/setSearchPhrase'
+    }),
     search () {
-
+      if (this.validate()) {
+        this.setPhrase(this.phrase)
+      }
+    },
+    validate () {
+      return this.phrase.length > 0
     }
   }
 }
@@ -32,6 +42,7 @@ export default {
   section {
     position: relative;
   }
+
   #search-input {
     border: 0;
     border-bottom: 1px solid $grey-accent;
@@ -40,16 +51,19 @@ export default {
     min-height: 40px;
     font-size: 1.2em;
     font-weight: 300;
+
     &::placeholder {
       text-transform: uppercase;
       text-align: center;
     }
+
     &:focus {
       border: 0;
       outline: none;
       border-bottom: 2px solid $secondary;
     }
   }
+
   .search-icon {
     position: absolute;
     right: 10px;
