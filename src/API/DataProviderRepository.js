ae8aa57bb024e4c13fd2b4b637993583d2db88ae
Kacper Orzechowski <kacper.orzechowski96@gmail.com>
2019-06-03 07:20:08 +0200

    add base functionality to get images and display them

---
+++ src/API/DataProviderRepository.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const DataProvider = axios.create({
+  /**
+   * This should not be here actually. I decided to keep host as it is for simplicity.
+   */
+  baseURL: 'http://localhost:5000/api/v1/images'
+})
+
+export default {
+  /**
+   * Gets images from Data Provider Service
+   * @returns {Promise<AxiosResponse<T>>}
+   */
+  get () {
+    return DataProvider.get('')
+  },
+  /**
+   * Searches images with given phrase in Data Provider Service
+   * @param phrase
+   * @returns {Promise<AxiosResponse<T>>}
+   */
+  search (phrase) {
+    return DataProvider.get(`/search/${phrase}`)
+  }
+}
---
+++ src/components/ImageGallery/VImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-lazy-image
+    :src="url"
+    src-placeholder="@/assets/images/blackhole.jpg"
+  />
+</template>
+
+<script>
+export default {
+  name: 'VImage',
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 100%;
+  }
+  .v-lazy-image {
+    filter: blur(10px);
+    transition: filter 0.7s;
+  }
+  .v-lazy-image-loaded {
+    filter: blur(0);
+  }
+</style>
